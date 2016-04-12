5c69f3367e62547e9e15e3aaf6e3faab4d4e2689
BrunoDeNadaiSarnaglia <brunodenadaisarnaglia@gmail.com>
2016-04-12 00:35:27 -0300

    GET request to node API

--- app/src/main/java/com/bruno/marco/maromba/LoginActivity.java
+++ app/src/main/java/com/bruno/marco/maromba/LoginActivity.java
@@ -1,9 +1,44 @@
 package com.bruno.marco.maromba;
 
+import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoDatabase;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.net.ssl.HttpsURLConnection;
+
 
 public class LoginActivity extends ActionBarActivity {
 
@@ -13,7 +48,36 @@ public class LoginActivity extends ActionBarActivity {
         setContentView(R.layout.activity_login);
     }
 
-    public void login (View v) {
-        startActivity(new Intent(this, MainActivity.class));
+    public void login(View v) {
+//        startActivity(new Intent(this, MainActivity.class));
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="https://maromba-server.herokuapp.com/api";
+
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        Context context = getApplicationContext();
+                        CharSequence text = response;
+                        int duration = Toast.LENGTH_SHORT;
+
+                        Toast toast = Toast.makeText(context, text, duration);
+                        toast.show();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Context context = getApplicationContext();
+                CharSequence text = error.toString();
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
 }
